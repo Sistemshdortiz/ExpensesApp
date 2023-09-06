b1ec57ef1cd1ff2f3f3cd37c69974e5eea7491ee
Sistemshdortiz <90852224+Sistemshdortiz@users.noreply.github.com>
2023-09-06 22:29:17 +0200

    Added dinamic list

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import { isMobile } from 'react-device-detect';
 
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title);
-    console.log("expenses items evalueted by react")
-
-    const ClickHandler = () => {
-        setTitle("Updated");
-
-        console.log(title);
-    };
 
     return (
         isMobile ?
             <Card className='expense-item'>
                 <ExpenseDate date={props.date} />
                 <div className='expense-item__description'>
-
+                        <h2>{props.title}</h2>
                     <div className='expense-item__price'>{props.amount}€</div>
                 </div>
             </Card>
@@ -27,10 +19,9 @@ const ExpenseItem = (props) => {
             <Card className='expense-item'>
                 <ExpenseDate date={props.date} />
                 <div className='expense-item__description'>
-                    <h2>{title}</h2>
-                    <div className='expense-item__price'>{props.amount}€</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>{props.amount}€</div>
                 </div>
-                <button onClick={ClickHandler}>Change Title</button>
             </Card>
     );
 }
